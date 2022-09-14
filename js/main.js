8e2f05eb044ba115b2fb4bc34ec0909f53c6dd64
ABunderson <76891486+ABunderson@users.noreply.github.com>
2022-09-14 15:44:21 -0600

    move parent const

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const links = [ {"label": "Week 1 code", "url": "week1/index.html"}];
 
-const parent = document.getElementById("index_list")
+
 
 for(let i = 0; i < links.length; i++){
-    console.log(links[i]['label']);
+    const parent = document.getElementById("index_list");
     var list_item = document.createElement("li");
     var list_link = document.createElement("a");
     parent.appendChild(list_item);
