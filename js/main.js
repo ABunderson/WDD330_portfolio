d0c04dbf45387404a97d72880af79825a4a78233
ABunderson <76891486+ABunderson@users.noreply.github.com>
2022-09-14 16:08:09 -0600

    move script line

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-const links = [ {"label": "Week 1 code", "url": "week1/index.html"}];
+const links = [{
+    "label": "Week 1 code",
+    "url": "week1/index.html"
+}];
 
-
-
-for(let i = 0; i < links.length; i++){
+for (let i = 0; i < links.length; i++) {
+    
     const parent = document.getElementById("index_list");
     var list_item = document.createElement("li");
     var list_link = document.createElement("a");
+
     parent.appendChild(list_item);
     list_item.appendChild(list_link);
     list_link.href = links[i]['url'];
