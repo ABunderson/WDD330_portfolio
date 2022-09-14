4056267760b536c02cc144adcd0d55139b16768a
ABunderson <76891486+ABunderson@users.noreply.github.com>
2022-09-14 16:29:11 -0600

    all items to week 1

--- week1/js/main.js
+++ week1/js/main.js
@@ -1,11 +1,14 @@
-const links = [ {"label": "Week 1 code", "url": "week1/index.html"}];
+const links = [{
+    "label": "Exercise 1",
+    "url": "exercise1.html"
+}];
 
-const parent = document.getElementById("index_list")
-
-for(let i = 0; i < links.length; i++){
-    console.log(links[i]['label']);
+for (let i = 0; i < links.length; i++) {
+    
+    const parent = document.getElementById("index_list");
     var list_item = document.createElement("li");
     var list_link = document.createElement("a");
+
     parent.appendChild(list_item);
     list_item.appendChild(list_link);
     list_link.href = links[i]['url'];
