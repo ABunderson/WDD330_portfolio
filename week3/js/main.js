68c61a193a4a1398b1b43a303b839faf36230ab3
ABunderson <76891486+ABunderson@users.noreply.github.com>
2022-09-27 21:56:08 -0600

    Week 3 start

--- week3/js/main.js
+++ week3/js/main.js
@@ -1,19 +1,13 @@
 const links = [{
-    "label": "Week 1",
-    "url": "week1/index.html"
-}, {
-    "label": "Week 2",
-    "url": "week2/index.html"
-}, {
-    "label": "Week 3",
-    "url": "week3/index.html"
+    "label": "Exercise 1",
+    "url": "exercise1.html"
 }];
 
 for (let i = 0; i < links.length; i++) {
     
     const parent = document.getElementById("index_list");
-    let list_item = document.createElement("li");
-    let list_link = document.createElement("a");
+    var list_item = document.createElement("li");
+    var list_link = document.createElement("a");
 
     parent.appendChild(list_item);
     list_item.appendChild(list_link);
